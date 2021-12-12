0ddd8c1e8d8fea237beeebabdd7ba835d654b711
Vitor Gois <82850418+Vit0rg@users.noreply.github.com>
2021-12-11 22:54:48 -0300

    Add files via upload
    A lot of changes.

--- main.js
+++ main.js
@@ -2,7 +2,19 @@ function cleanData()
 {
   let input = document.getElementById("texto_original").value;
   let lixo = document.getElementById("lixo").value;
-  let output = input.split(lixo).join('');
+  var output = input.split(lixo).join('');
 
   document.getElementById("texto_filtrado").innerHTML = output;
+}
+
+function Copy()
+{
+  let copiarTexto = document.getElementById("texto_filtrado");
+
+  navigator.clipboard.writeText(copiarTexto.innerHTML);
+}
+
+function Reset()
+{
+  document.getElementById("texto_filtrado").innerHTML = '';
 }
